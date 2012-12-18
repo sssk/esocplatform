0ba86379469511cb085d0adb8b9261f6059a1ac3
sssk <skinawy@gmail.com>
2012-12-17 19:58:09 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esocplatform::Application.config.secret_token = '1203c83a174eabce904d8d359776b71b0a77c48aaa29489d4c8598eefb4896994fe0ec036d2aa28fef4463c796c6fac104d4d5bc430d9c178eaa0396844362be'
